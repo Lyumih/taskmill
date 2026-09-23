0a707c31f8569e369620f33f411e4e792f468c34
Lyumih <m.ivanov1225@gmail.com>
2026-09-23 19:04:10 +0300

    добавлена начало

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider } from 'antd'
+import ruRU from 'antd/locale/ru_RU'
 import './index.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider
+      locale={ruRU}
+      theme={{ token: { colorPrimary: '#3156d8', borderRadius: 10 } }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ConfigProvider>
   </StrictMode>,
 )
